e6e7d5a3b78a4e059a87e1d02cc59402e62b9cd7
Mission Bit <missionbit@lightcoral-option.local>
2014-06-03 19:09:21 -0700

    initial commit

---
+++ android/src/com/missionbit/megajumper/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.missionbit.megajumper.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.missionbit.megajumper.MegaJumper;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MegaJumper(), config);
+	}
+}
