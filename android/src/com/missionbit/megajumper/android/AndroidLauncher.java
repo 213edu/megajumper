50dc24a16f189d08bdf6d9749ea5dd669341fdb6
Mission Bit <missionbit@lightcoral-option.local>
2014-06-03 19:21:51 -0700

    replace tabs with spaces

--- android/src/com/missionbit/megajumper/android/AndroidLauncher.java
+++ android/src/com/missionbit/megajumper/android/AndroidLauncher.java
@@ -7,10 +7,10 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.missionbit.megajumper.MegaJumper;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MegaJumper(), config);
-	}
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new MegaJumper(), config);
+    }
 }
