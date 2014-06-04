80add2d62bab29504ba75a2456c4a0c7980e37ca
Mission Bit <missionbit@lightcoral-option.local>
2014-06-03 19:41:56 -0700

    add {reset,update,draw}Game methods

--- core/src/com/missionbit/megajumper/MegaJumper.java
+++ core/src/com/missionbit/megajumper/MegaJumper.java
@@ -7,19 +7,39 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MegaJumper extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
+    private SpriteBatch batch;
+    private Texture img;
+    private int width;
+    private int height;
 
     @Override
     public void create () {
         batch = new SpriteBatch();
         img = new Texture("missionbit.png");
+        width = Gdx.graphics.getWidth();
+        height = Gdx.graphics.getHeight();
+
+        resetGame();
     }
 
     @Override
     public void render () {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        updateGame();
+        drawGame();
+    }
+
+    private void resetGame() {
+        //reset any game state variables here
+    }
+
+    private void updateGame() {
+        //apply all game rules here and check for win or loss
+    }
+
+    private void drawGame() {
         batch.begin();
         batch.draw(img, 0, 0);
         batch.end();
